eebab7081f7abfe52d624b7dc4133751eb209eb2
Fabeure <sazaouzi@gmail.com>
2024-02-22 02:30:26 +0100

    use my own deploy

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 className="text-3xl font-bold underline">aaa</h1>
+      <h1 className="text-3xl font-bold underline">Fabeure</h1>
       <div className="card">
         <button onClick={() => {
           setCount((count) => count + 1)
