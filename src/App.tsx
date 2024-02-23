2ba9d38887ec37675a1df81a95ce4f4cb4a35787
Fabeure <sazaouzi@gmail.com>
2024-02-23 08:34:29 +0100

    api call

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,9 @@ function App() {
       <button onClick={() => {
           setCount((count) => count + 1)
           fetch('https://corsproxy.io/?https://studyfiedbackend.onrender.com/WeatherForecast')
-      .then(response => alert(response.json()))
+      .then(response => {
+        console.log(response.text)
+        console.log(response)})
       .catch(error => console.error(error));
           }}>
           count is {count}
