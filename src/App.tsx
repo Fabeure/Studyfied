87c684a4c2281fab4f5ab2bb60b6b3526f6d4c77
Saber Azzouzi <saber.azzouzi@expensya.com>
2024-02-29 17:57:34 +0100

    typo

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 className="text-3xl font-bold underline">{import.meta.env.VITE_TEST_VAR}</h1>
+      <h1 className="text-3xl font-bold underline">salem</h1>
       <div className="card">
       <button onClick={() => {
           setCount((count) => count + 1);
