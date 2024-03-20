b9dd9fe97cdb32c5e11a4ac7b285a3c250f5ebdf
Zied <ziedmaghraoui58@gmail.com>
2024-03-20 15:07:25 +0100

    base login skeleton

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Home work in progress 🏗</h1>
     </div>
   );
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,9 +3,84 @@
 export default function Login() {
   return (
     <div className="page-content">
-      <h1>Login Page</h1>
-      <button onClick={() => console.log("Login clicked")}>Login</button>
-      <button onClick={() => console.log("Register clicked")}>Register</button>
+      <h1 style={{ marginTop: "1rem", marginBottom: "4rem" }}>
+        Don't have an account ? Sign up
+      </h1>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          width: "60%",
+        }}
+      >
+        {/* ///////////// register wrapper */}
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "start",
+            width: "49%",
+          }}
+        >
+          <h1>New here</h1>
+          <label htmlFor="">email</label>
+          <input
+            type="text"
+            name=""
+            id=""
+            style={{ border: "solid black 1px", margin: "0.2rem" }}
+          />
+          <label htmlFor="">password</label>
+          <input
+            type="text"
+            name=""
+            id=""
+            style={{ border: "solid black 1px", margin: "0.2rem" }}
+          />
+          <label htmlFor="">confirm password</label>
+          <input
+            type="text"
+            name=""
+            id=""
+            style={{ border: "solid black 1px", margin: "0.2rem" }}
+          />
+          <button onClick={() => console.log("Register clicked")}>
+            Register
+          </button>
+        </div>
+        {/* ////////// divider */}
+        <div
+          style={{ backgroundColor: "violet", width: "1px", margin: "1rem" }}
+        ></div>
+        {/* /////////////// login wrapper */}
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            height: "fit-content",
+            alignItems: "start",
+            width: "49%",
+          }}
+        >
+          <h1>Already a member</h1>
+          <label htmlFor="">email</label>
+          <input
+            type="text"
+            name=""
+            id=""
+            style={{ margin: "0.2rem", border: "solid black 1px" }}
+          />
+          <label htmlFor="">password</label>
+          <input
+            type="text"
+            name=""
+            id=""
+            style={{ margin: "0.2rem", border: "solid black 1px" }}
+          />
+          <button onClick={() => console.log("Login clicked")}>Login</button>
+        </div>
+      </div>
     </div>
   );
 }
