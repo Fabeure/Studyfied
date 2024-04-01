0908252fd05159c8b84c16909c0717e7b1f39f39
farahBassoumi <bassoumiifarah@gmail.com>
2024-04-01 05:05:14 +0100

    icons

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./HomePage.css";
 
-import { Container } from "@mui/material";
+import { Container, iconButtonClasses } from "@mui/material";
 import addSVG from "../../assets/addgrey.png";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
@@ -10,49 +10,26 @@ import CardContent from "@mui/material/CardContent";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import Typography from "@mui/material/Typography";
-import "./HomePage.css";
-
-const L=4;
+//import{ quiz, flashCards, summary, explain} from "../../assets/illustrations";
+import quiz from "../../assets/illustrations/quiz.png";
+import flashCards from "../../assets/illustrations/flashCards.png";
+import summary from "../../assets/illustrations/summary.png";
+import explain from "../../assets/illustrations/explain.png";
+import { setsFromData } from "./data";
+const L = 4;
 function Home() {
-
-  const [sets, setSets] = React.useState([
-    {
-      id: "1",
-      topic: "Summary",
-      description:
-      "Effortlessly condense long texts into concise summaries for quick understanding."
-        },
-    {
-      id: "2",
-      topic: "Flash cards",
-      description: "Create interactive study aids from any content to enhance learning and retention."
-    },
-    {
-      id: "3",
-      topic: "Quiz",
-      description: "Engage users with customized quizzes that test knowledge and promote active learning.",
-    },
-    {
-      id: "4",
-      topic: "Explain",
-      description:
-        "Gain deep insights and understanding with AI-generated explanations.",
-    },
-  ]);
+  const [sets, setSets] = React.useState(setsFromData);
 
   const [startingIndex, setStartingIndex] = useState(0);
   const [disableNextBtn, setDisableNextBtn] = useState(false);
   const [disablePrevBtn, setDisablePrevBtn] = useState(true);
   const [currentSets, setCurrentSets] = useState(sets.slice(0, 1));
 
-  useEffect(
-    () => {
-      setCurrentSets(sets.slice(startingIndex, startingIndex + 1));
-      setDisableNextBtn(startingIndex >= L - 1);
-      setDisablePrevBtn(startingIndex == 0);
-    },
-    [startingIndex]
-  );
+  useEffect(() => {
+    setCurrentSets(sets.slice(startingIndex, startingIndex + 1));
+    setDisableNextBtn(startingIndex >= L - 1);
+    setDisablePrevBtn(startingIndex == 0);
+  }, [startingIndex]);
 
   function handleArrowClick(event: React.MouseEvent<HTMLButtonElement>): void {
     const btn = event.currentTarget.name;
@@ -64,52 +41,29 @@ function Home() {
     }
   }
 
-  // const bull = (
-  //   <Box
-  //     component="span"
-  //     sx={{
-  //       bgcolor: "tomato",
-  //       display: "inline-block",
-  //       mx: "7px",
-  //       transform: "scale(0.8)",
-  //     }}
-  //   >
-  //     •
-  //   </Box>
-  // );
-
   return (
-    <div className="page-content m-5">
+    <div className="page-content ">
       {sets.length !== 0 ? (
         <>
           <div className="sets-container">
-
-
-          <div className="navigation-buttons" >
-            <button
-              className="Scroll-Btn Prev-Btn"
-              name="prevButton"
-              disabled={disablePrevBtn}
-              onClick={handleArrowClick}
-            >
-              <ArrowBackIosNewIcon className="arrow-icon" />
-            </button>
-            <button
-              className="Scroll-Btn Next-Btn"
-              name="nextButton"
-              disabled={disableNextBtn}
-              onClick={handleArrowClick}
-            >
-              <ArrowForwardIosIcon  className="arrow-icon" />
-            </button>
-          </div>
-
-
-
-
-
-
-
+            <div className="navigation-buttons">
+              <button
+                className="Scroll-Btn Prev-Btn"
+                name="prevButton"
+                disabled={disablePrevBtn}
+                onClick={handleArrowClick}
+              >
+                <ArrowBackIosNewIcon className="arrow-icon" />
+              </button>
+              <button
+                className="Scroll-Btn Next-Btn"
+                name="nextButton"
+                disabled={disableNextBtn}
+                onClick={handleArrowClick}
+              >
+                <ArrowForwardIosIcon className="arrow-icon" />
+              </button>
+            </div>
 
             <div
               id="slider"
@@ -127,8 +81,8 @@ function Home() {
                         border: "1px solid #f3f2f2",
                         borderRadius: 7,
                         boxShadow: " 0px 5px 6px 2px rgba(0, 0, 0, 0.3)",
-                        width: {md:650,sm:400, xs:250},
-                        height: {m:300,sm:200, xs:150},
+                        width: { md: 650, sm: 400, xs: 250 },
+                        height: { m: 300, sm: 200, xs: 150 },
                       }}
                       className="card"
                     >
@@ -142,9 +96,7 @@ function Home() {
                             height: "150px",
                           }}
                         >
-                          <p className="card-topic" >
-                            {set.topic}
-                          </p>
+                          <p className="card-topic">{set.topic}</p>
 
                           <Typography
                             sx={{
@@ -158,9 +110,7 @@ function Home() {
                         </CardContent>
 
                         <div className="learnmore-container">
-                          <button className="learn-more" >
-                            Learn more
-                          </button>
+                          <button className="learn-more">Try it now !</button>
                         </div>
                       </React.Fragment>
                     </Card>
@@ -168,8 +118,6 @@ function Home() {
                 );
               })}
             </div>
-         
-         
           </div>
           {/**/}
         </>
@@ -186,33 +134,38 @@ function Home() {
           className="add-card"
         >
           <React.Fragment>
-            <Box sx={{}}>
-              <CardContent>
-                <div className="add-icon-container">
-                  <img className="add-icon" src={addSVG} alt="Icon" />
-                </div>
-              </CardContent>
-              <CardContent
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  width: "100%",
-                }}
-              >
-                <Typography
+            <div className="card-content">
+              <div className="left-content">
+                <img src="{set.icon}" alt="" />
+              </div>
+              <div className="right-card-content">
+                <CardContent>
+                  <div className="add-card-icon-container">
+                    <img className="add-icon" src={addSVG} alt="Icon" />
+                  </div>
+                </CardContent>
+                <CardContent
                   sx={{
-                    mb: 1,
-                    fontSize: "medium",
-                    marginTop: 5,
-                    fontStyle: "italic",
-                    color: "rgba(0, 0, 0, 0.6)",
+                    display: "flex",
+                    flexDirection: "column",
+                    width: "100%",
                   }}
-                  color="text.secondary"
                 >
-                  create a new set
-                </Typography>
-              </CardContent>
-            </Box>
+                  <Typography
+                    sx={{
+                      mb: 1,
+                      fontSize: "medium",
+                      marginTop: 5,
+                      fontStyle: "italic",
+                      color: "rgba(0, 0, 0, 0.6)",
+                    }}
+                    color="text.secondary"
+                  >
+                    create a new set
+                  </Typography>
+                </CardContent>
+              </div>
+            </div>
             <CardActions></CardActions>
           </React.Fragment>
         </Card>
--- src/pages/Home/data.ts
+++ src/pages/Home/data.ts
@@ -2,68 +2,38 @@ export interface SetType {
   id: string;
   topic: string;
   description: string;
-  creationTime: string;
+  icon:string
 }
 
-export const sets: SetType[] = [
+export const setsFromData: SetType[] = [
   {
     id: "1",
-    topic: "Maths",
-    description: "Maths is the study of numbers, shapes and patterns",
-    creationTime: "2021-10-10T14:48:00",
+    topic: "Summary",
+    description:
+    "Effortlessly condense long texts into concise summaries for quick understanding."
+,
+      icon: "summary",
   },
   {
     id: "2",
-    topic: "Science",
-    description: "Science is the study of the world around us",
-    creationTime: "2021-10-10T14:48:00",
+    topic: "Flash cards",
+    description: "Create interactive study aids from any content to enhance learning and retention.",
+
+  icon: "flashCards"
   },
   {
     id: "3",
-    topic: "English",
-    description: "English is the study of the English language",
-    creationTime: "2021-10-10T14:48:00",
-  },
+    topic: "Quiz",
+    description: "Engage users with customized quizzes that test knowledge and promote active learning.",
+ 
+  icon: "quiz", 
+},
   {
     id: "4",
-    topic: "History",
-    description: "History is the study of the past",
-    creationTime: "2021-10-10T14:48:00",
-  },
-  {
-    id: "5",
-    topic: "Geography",
-    description: "Geography is the study of the earth",
-    creationTime: "2021-10-10T14:48:00",
-  },
-  {
-    id: "6",
-    topic: "Art",
-    description: "Art is the study of creativity",
-    creationTime: "2021-10-10T14:48:00",
-  },
-  {
-    id: "7",
-    topic: "Music",
-    description: "Music is the study of sound",
-    creationTime: "2021-10-10T14:48:00",
-  },
-  {
-    id: "8",
-    topic: "Physical Education",
-    description: "Physical Education is the study of physical fitness",
-    creationTime: "2021-10-10T14:48:00",
-  },
-  {
-    id: "9",
-    topic: "Religious Education",
-    description: "Religious Education is the study of religion",
-    creationTime: "2021-10-10T14:48:00",
-  },
-  {
-    id: "10",
-    topic: "Computing",
-    description: "Computing is the study of computers",
-    creationTime: "2021-10-10T14:48:00",
-  },
-];
+    topic: "Explain",
+    description:
+      "Gain deep insights and understanding with AI-generated explanations.",
+
+  icon: "explain",
+},
+]
