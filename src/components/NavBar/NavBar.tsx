9721a6a786086da5957144ea2180cdb3b103068b
farahBassoumi <bassoumiifarah@gmail.com>
2024-04-28 01:15:27 +0100

    navbar UI + background

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,101 +1,30 @@
-import { ReactNode } from "react";
-import { NavLink } from "react-router-dom";
 import "./NavBar.css";
-import logo from "../../assets/studyfast.svg";
-import { Button, Grid } from "@mui/material";
-import useAuth from "../../hooks/useAuth";
 
-interface LinkType {
-  name: string; // Name of the link
-  path: string; // Path associated with the link
-  element: ReactNode; // Type of the React component to render
-}
-interface NavBarProps {
-  links: LinkType[];
-}
+import logoLeaf from "../../assets/logo_leaf.png";
 
-const NavBar: React.FC<NavBarProps> = ({ links }) => {
-  const loginPath = links.find(({ name }) => name == "Login")?.path;
-  const { user, setUser } = useAuth();
-
+function NavBar() {
   return (
     <>
-      <nav className="navbar">
-        <ul className="linkGroup">
-          <div className="logo routeName ">
-            <img className="logoImage" src={logo} alt="My Photo" />
+      <nav className="navbar flex flex-end w-full">
+        <div className="left-container flex items-center w-full mr-[20px] md:ml-[40px]  xl:ml-[60px]  h-[4rem]">
+          <div className="logo-name sm:px-[10px]  md:px-[30px] xl:px-[60px] h-[3rem] flex flex-row items-center   ">
+            <div className="logo ml-[10px] ">
+              <img className="logoImage" src={logoLeaf} alt="studyfluxLOGO" />
+            </div>
+            <div className="name font-semibold  ">STUDYFLUX</div>
           </div>
-          {links
-            .filter((route) => route.name != "Login")
-            .map((route, key) => (
-              <li className="linkWrapper" key={key}>
-                <NavLink
-                  to={route.path}
-                  end
-                  className={({ isActive }) =>
-                    `routeName ${isActive ? "active" : "inactive"}`
-                  }
-                >
-                  {route.name}
-                </NavLink>
-              </li>
-            ))}
-        </ul>
-
-        {user.accessToken != "" && (
-          <Grid
-            container
-            // bgcolor={"red"}
-            direction={"row"}
-            justifyContent={"end"}
-            alignItems={"center"}
-            columnGap={3}
-          >
-            <Grid item>Hello : {user.email}</Grid>
-            <Grid item>
-              <NavLink to="/Studyfied">
-                <Button
-                  variant="contained"
-                  onClick={() => {
-                    setUser({ accessToken: "", email: "", userId: "" });
-                  }}
-                  sx={{
-                    // width: "100%",
-                    fontSize: "1rem",
-                    padding: "0.7rem",
-                    borderRadius: "1.5rem",
-                    width:"7rem",
-                    boxShadow: 0,
-                    backgroundColor: "black",
-                    color: "secondary.light",
-                    textTransform: "none",
-                    ["&:hover"]: {
-                      color: "secondary.light",
-                      boxShadow: 0,
-                      backgroundColor: "#3a3a3a",
-                    },
-                  }}
-                >
-                  Log out
-                </Button>
-              </NavLink>
-            </Grid>
-          </Grid>
-        )}
-        {loginPath && user.accessToken == "" && (
-          <NavLink
-            to={loginPath}
-            className={({ isActive }) =>
-              `loginLink ${isActive ? "active" : "inactive"}`
-            }
-          >
-            Login
-          </NavLink>
-        )}
+        </div>
+        <div className="left-container flex items-center row-reverse mr-[20px] md:mr-[100px] xl:mr-[150px] ">
+         <button className="button  flex  justify-center items-center h-[29px] w-[100px] md:w-[110px] lg:w-[120px]  xl:w-[130px] mr-[10px] text-[10px] md:text-[12px]  text-uppercase ">
+          sign in
+         </button>
+         <button className="button  flex  justify-center items-center h-[29px] w-[100px] md:w-[110px] lg:w-[120px]  xl:w-[130px] mr-[10px] text-[10px] md:text-[12px]  text-uppercase ">
+          get a demo
+         </button>
+        </div>
       </nav>
-      <div className="separator"></div>
     </>
   );
-};
+}
 
 export default NavBar;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,151 +1,53 @@
-import React, { useState, useEffect } from "react";
 import "./HomePage.css";
-import logo from "../../assets/studyfast.svg";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
-import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
-import Typography from "@mui/material/Typography";
-import quiz from "../../assets/illustrations/quiz.png";
-import flashCards from "../../assets/illustrations/flashCards.png";
-import summary from "../../assets/illustrations/summary.png";
-import explain from "../../assets/illustrations/explain.png";
-import { setsFromData } from "./data";
-const L = 4;
+
 function Home() {
-  const sets = setsFromData;
-  const [startingIndex, setStartingIndex] = useState(0);
-  const [disableNextBtn, setDisableNextBtn] = useState(false);
-  const [disablePrevBtn, setDisablePrevBtn] = useState(true);
-  const [currentSets, setCurrentSets] = useState(sets.slice(0, 1));
-
-  useEffect(() => {
-    setCurrentSets(sets.slice(startingIndex, startingIndex + 1));
-    setDisableNextBtn(startingIndex >= L - 1);
-    setDisablePrevBtn(startingIndex == 0);
-  }, [startingIndex]);
-
-  function handleArrowClick(event: React.MouseEvent<HTMLButtonElement>): void {
-    const btn = event.currentTarget.name;
-    if (btn === "prevButton") {
-      setStartingIndex(Math.max(startingIndex - 1, 0));
-    }
-    if (btn === "nextButton") {
-      setStartingIndex(Math.min(startingIndex + 1, L));
-    }
-  }
-
   return (
-    <div className="page-content flex items-center ">
-        <div className="introduction-container flex items-center flex-col justify-center ">
-          <div className="logo-name flex flex-row md:gap-10 gap-3 md:text-6xl sm:text-4xl text-3xl sm:gap-5 ">
-            <div className="logo-container">
-              <img className="logo"  src={logo} alt="logo" />
-            </div>
-            <div className="name  ">Brain Wave</div>
-             </div>
-          <div className="slogan md:text-3xl sm:text-2xl text-1.5xl">
-          Unlock the future of learning with AI insights!
-          </div>
-
-          <div className="description md:text-[18px] sm:text-[14px] text-[11px]">
-            "Revolutionize your learning experience with our dynamic platform!
-            Generate personalized flashcards, quizzes, and summaries
-            effortlessly. Engage in insightful conversations with AI chatbots.
-            Elevate your knowledge acquisition like never before!"
-          </div>
-        </div>
-      <div className="features-container ">
-        <div className="navigation-buttons">
-          <button
-            className="Scroll-Btn Prev-Btn"
-            name="prevButton"
-            disabled={disablePrevBtn}
-            onClick={handleArrowClick}
-          >
-            <ArrowBackIosNewIcon className="arrow-icon" />
-          </button>
-          <button
-            className="Scroll-Btn Next-Btn"
-            name="nextButton"
-            disabled={disableNextBtn}
-            onClick={handleArrowClick}
-          >
-            <ArrowForwardIosIcon className="arrow-icon" />
-          </button>
-        </div>
-        <div
-          id="slider"
-          className="flex flex-row space-x-7 justify-center card-container  "
-          style={{ position: "relative"
-          }}
-        >
-          {currentSets.map((set) => {
-            let icon = "";
-
-            switch (set.icon) {
-              case "summary":
-                icon = summary;
-                break;
-              case "explain":
-                icon = explain;
-                break;
-              case "quiz":
-                icon = quiz;
-                break;
-              default:
-                icon = flashCards;
-                break;
+    <>
+      <svg
+        className="w-full  blur-md "
+        preserveAspectRatio="xMidYMid slice"
+        viewBox="10 10 80 80"
+      >
+        <defs>
+          <style>
+            {`
+            @keyframes rotate {
+					 0% {
+                    transform: rotate(0deg);
+                }
+                100% {
+                    transform: rotate(360deg);
+                }
             }
-            return (
-              <div key={set.id}>
-                <Card
-                  variant="outlined"
-                  sx={{
-                    border: "1px solid #f3f2f2",
-                    borderRadius: 7,
-                    boxShadow: " 0px 5px 6px 2px rgba(0, 0, 0, 0.3)",
-                    width: {lg:750, md: 550, sm: 400, xs: 350 },
-                    height: { lg:300,md: 250, sm: 200, xs: 200 },
-                  }}
-                  className="card flex items-center justify-center "
-                >
-                  <div className="left-card-content  flex items-center justify-center  ">
-                    <img src={icon} alt="icon" className="card-icon" />
-                  </div>
-                  <div className="right-card-content ">
-                    <CardContent
-                      sx={{
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "flex-start",
-                        width: "100%",
-                      }}
-                    >
-                      <p className="card-topic">{set.topic}</p>
+            .out-top {
+              animation: rotate 8s linear infinite;
+              transform-origin: 13px 25px;
+          }
+          .in-top {
+              animation: rotate  8s linear infinite;
+              transform-origin: 13px 25px;
+          }
+          .out-bottom {
+              animation: rotate  15s linear infinite;
+              transform-origin: 84px 93px;
+          }
+          .in-bottom {
+              animation: rotate  8s linear infinite;
+              transform-origin: 84px 93px;
+          }
+            `}
+          </style>
+        </defs>
+       
+        <path fill="#02071B " className="out-top opacity-80" d="M37-5C25.1-14.7,5.7-19.1-9.2-10-28.5,1.8-32.7,31.1-19.8,49c15.5,21.5,52.6,22,67.2,2.3C59.4,35,53.7,8.5,37-5Z"/>
+    <path fill="#020A25 " className="in-top opacity-80" d="M20.6,4.1C11.6,1.5-1.9,2.5-8,11.2-16.3,23.1-8.2,45.6,7.4,50S42.1,38.9,41,24.5C40.2,14.1,29.4,6.6,20.6,4.1Z"/>
+    <path fill="#02071B " className="out-bottom opacity-80" d="M105.9,48.6c-12.4-8.2-29.3-4.8-39.4.8-23.4,12.8-37.7,51.9-19.1,74.1s63.9,15.3,76-5.6c7.6-13.3,1.8-31.1-2.3-43.8C117.6,63.3,114.7,54.3,105.9,48.6Z"/>
+    <path fill="#020A25 " className="in-bottom opacity-80" d="M102,67.1c-9.6-6.1-22-3.1-29.5,2-15.4,10.7-19.6,37.5-7.6,47.8s35.9,3.9,44.5-12.5C115.5,92.6,113.9,74.6,102,67.1Z"/>
+      </svg>
 
-                      <Typography
-                        sx={{
-                          textAlign: "left",
-                          overflow: "hidden",
-                        }}
-
-                      >
-                        {set.description}
-                      </Typography>
-                    </CardContent>
-
-                    <div className="learnmore-container">
-                      <button className="learn-more">Try it now !</button>
-                    </div>
-                  </div>
-                </Card>
-              </div>
-            );
-          })}
-        </div>
-      </div>
-    </div>
+      <div className="z-10  h-[1000px]"> holdfghjk,l;</div>
+      <div className=" z-10 h-[1000px]"> holdfghjk,l;</div>
+    </>
   );
 }
 export default Home;
