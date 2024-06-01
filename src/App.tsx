f1d8ed04f7e1b89ae29d0b6c53e1beea4be2b0f7
Saber Azzouzi <saber.azzouzi@expensya.com>
2024-06-01 22:57:51 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { HashRouter } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/Register/ResgisterPage";
