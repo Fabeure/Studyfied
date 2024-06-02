4085b81d8ce6215b5720d1901c95300891f12fab
farahBassoumi <bassoumiifarah@gmail.com>
2024-06-02 23:52:42 +0100

    update savedFlashCards-Ui

--- src/pages/SavedFlashCards/SavedFlashCards.tsx
+++ src/pages/SavedFlashCards/SavedFlashCards.tsx
@@ -1,25 +1,16 @@
-import { Grid, Paper, styled } from '@mui/material';
-import { useState, useEffect, useContext } from 'react';
+import { useState, useEffect, useContext } from "react";
 import AuthContext from "../../context/AuthProvider";
-import { useNavigate } from 'react-router-dom';
-import './SavedFlashCards.css'
-import axios from 'axios';
-import { FlashcardModel } from '../../models/flashcardModel';
-
-const Item = styled(Paper)(({ theme }) => ({
-    ...theme.typography.body2,
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-    height: 60,
-    lineHeight: '60px',
-  }));
+import { useNavigate } from "react-router-dom";
+import "./SavedFlashCards.css";
+import axios from "axios";
+import { FlashcardModel } from "../../models/flashcardModel";
 
 const getSavedFlashCardsEndpoint = `${process.env.VITE_BACKEND_API}/api/FlashCards/getFlashCardsByUserId`;
-  
+
 function SavedFlashCards() {
-    const { user } = useContext(AuthContext);
-    const [savedSets, setSavedSets] = useState<FlashcardModel[]>([]);
-    const navigate = useNavigate();
+  const { user } = useContext(AuthContext);
+  const [savedSets, setSavedSets] = useState<FlashcardModel[]>([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -28,15 +19,19 @@ function SavedFlashCards() {
           userId: encodeURIComponent(user?.userId),
         };
 
-        const response = await axios.get(getSavedFlashCardsEndpoint, { params });
-        console.log(response.data)
+        const response = await axios.get(getSavedFlashCardsEndpoint, {
+          params,
+        });
+        console.log(response.data);
         const fetchedSets = response.data.resultItem as FlashcardModel[]; // Cast to FlashcardModel[] for type safety
-        console.log(fetchedSets)
+        console.log(fetchedSets);
         setSavedSets(fetchedSets);
-        console.log("result: ", savedSets)
+        console.log("result: ", savedSets);
       } catch (err) {
         console.error("Error fetching saved flash cards:", err);
-        alert("An error occurred while fetching your saved sets. Please check the console for details.");
+        alert(
+          "An error occurred while fetching your saved sets. Please check the console for details."
+        );
       }
     };
 
@@ -44,25 +39,24 @@ function SavedFlashCards() {
   }, [user]);
 
   return (
-    <div className='content'>
-    <Grid
-    container
-    direction={"row"}
-    columnGap={2}
-    rowGap={2}
-    justifyContent={"center"}>
-        {savedSets.map((savedSet) =>
-        <Grid
-        item
-        xs={3}
-        key={savedSet.id}>
-                <Item onClick={() => {navigate('/flashcards', {state:{savedFlashCard: savedSet}})}}>
-                  {Object.keys(savedSet.items)[0]}
-                  
-                </Item>
-        </Grid>
-        )}
-    </Grid>
+    <div className="content flex items-center justify-center">
+      <div className="backButton flex flex-wrap  items-center justify-center gap-[100px]  cursor-pointer">
+        {savedSets.map((savedSet) => (
+          <div
+            className="savedSet w-[400px] h-[150px] flex items-center justify-center  "
+            key={savedSet.id}
+            onClick={() => {
+              navigate("/flashcards", {
+                state: { savedFlashCard: savedSet },
+              });
+            }}
+          >
+            <div className="savedSetText  text-white w-[400px] h-[150px] flex items-center justify-center ">
+              <div>{Object.keys(savedSet.items)[0]}</div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
