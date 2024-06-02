508cf984d902016097d43ec572c24b94f5db1f42
Zied <ziedmaghraoui58@gmail.com>
2024-06-02 20:18:37 +0100

    chat page style

--- src/components/TextToSpeech/TextToSpeech.tsx
+++ src/components/TextToSpeech/TextToSpeech.tsx
@@ -1,26 +1,45 @@
-import { FormEvent, useContext, useState, useEffect } from "react";
+import { FormEvent, useContext, useState, useEffect, ChangeEvent } from "react";
 import { AppContext } from "../../context/IsPlayingContext";
-import axios from 'axios';
+import axios from "axios";
+import UploadButton from "../UploadButton/UploadButton";
+import { Button, CircularProgress, SxProps, Theme } from "@mui/material";
+import ChatMessage from "./ChatMessage";
+
+interface Message {
+  sender: "user" | "bot";
+  text: string;
+}
 
 const TextToSpeech = () => {
   const [userText, setUserText] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const { isPlaying, setIsPlaying } = useContext(AppContext);
   const [audioUrl, setAudioUrl] = useState("");
+  const [conversation, setConversation] = useState<Message[]>(fakeData);
+  const [file, setFile] = useState<File | null>(null);
 
   const generateSpeech = async (textToSpeak: string) => {
     try {
-      const response = await axios.post('http://localhost:3000/api/generate-speech', {
-        text: textToSpeak,
-        voice: "s3://voice-cloning-zero-shot/801a663f-efd0-4254-98d0-5c175514c3e8/jennifer/manifest.json"
-      }, { responseType: 'arraybuffer' });
+      const response = await axios.post(
+        "http://localhost:3000/api/generate-speech",
+        {
+          text: textToSpeak,
+          voice:
+            "s3://voice-cloning-zero-shot/801a663f-efd0-4254-98d0-5c175514c3e8/jennifer/manifest.json",
+        },
+        { responseType: "arraybuffer" }
+      );
 
-      const blob = new Blob([response.data], { type: 'audio/mpeg' });
+      const blob = new Blob([response.data], { type: "audio/mpeg" });
       return URL.createObjectURL(blob);
     } catch (error) {
-      console.error('Error generating speech:', error);
-      throw new Error('Failed to generate speech');
+      console.error("Error generating speech:", error);
+      throw new Error("Failed to generate speech");
     }
+  };
+
+  const getReply = (prompt: string) => {
+    return "placeholder bot reply";
   };
 
   const handleUserText = async (event: FormEvent) => {
@@ -28,14 +47,23 @@ const TextToSpeech = () => {
     if (userText === "") return alert("Please enter text");
     setIsLoading(true);
     try {
-      const userText = "Hello, I am a virtual assistant. How can I help you? GOOD job your a good developer.";
-      const audioUrl = await generateSpeech(userText);
+      const botReply = getReply(userText);
+      const audioUrl = await generateSpeech(botReply);
+      setConversation((prev) => [
+        ...prev,
+        { sender: "user", text: userText },
+        {
+          sender: "bot",
+          text: botReply,
+        },
+      ]);
       setAudioUrl(audioUrl);
       setIsPlaying(true);
     } catch (error) {
       let message = "";
       if (error instanceof Error) message = error.message;
       console.log(message);
+      alert("an error occured");
     } finally {
       setIsLoading(false);
       setUserText("");
@@ -52,34 +80,110 @@ const TextToSpeech = () => {
     }
   }, [audioUrl, isPlaying]);
 
+  const handleFileUpload = (event: ChangeEvent<HTMLInputElement>) => {
+    setFile(event.target.files ? event.target.files[0] : null);
+  };
+
   return (
-    <div className="relative top-0 z-50 ">
+    <div className="flex flex-col items-center h-full">
+      <div
+        className="overflow-y-auto p-4 w-full"
+        style={{ height: "calc(100vh - 4rem)", paddingBottom: "8rem" }}
+      >
+        <div className="w-2/3 mx-auto">
+          {conversation.map((message, index) => (
+            <ChatMessage
+              text={message.text}
+              sender={message.sender}
+              key={index}
+            />
+          ))}
+        </div>
+      </div>
+
       <form
         onSubmit={handleUserText}
-        className="absolute top-[800px] left-[30%] space-x-2 pt-2"
+        className="flex justify-center h-8rem w-full backdrop-blur-2xl  py-3"
+        style={{
+          position: "fixed",
+          bottom: 0,
+          background:
+            "linear-gradient(to right,rgba(250, 6, 173, 0),rgba(250, 6, 173, 0.2),rgba(158, 90, 249, 0.2),rgba(36, 5, 130, 0.2),rgba(36, 5, 130, 0))",
+        }}
       >
-        <input
-          type="text"
-          value={userText}
-          className="bg-transparent w-[510px] border border-[#b00c3f]/80 outline-none rounded-lg placeholder:text-[#b00c3f] p-2 text-[#b00c3f]"
-          onChange={(e) => setUserText(e.target.value)}
-          placeholder="What do you want to know human...."
-        />
-        <button
-          disabled={isLoading}
-          className="text-[#b00c3f] p-2 border border-[#b00c3f] rounded-lg disabled:text-blue-100 disabled:cursor-not-allowed disabled:bg-gray-500 hover:scale-110 hover:bg-[#b00c3f] hover:text-black duration-300 transition-all"
-        >
-          {isLoading ? "thinking..." : "Ask"}
-        </button>
+        <div className="flex gap-3 items-center p-2 w-3/4">
+          <div>
+            <UploadButton onChange={handleFileUpload} />
+            {file && <p className="ml-2">File selected: {file.name}</p>}
+          </div>
+          <div
+            style={{
+              background:
+                "linear-gradient(to right, rgb(187, 87, 254), rgb(112, 216, 222))",
+              padding: "4px",
+              borderRadius: "1em",
+              flex: "1",
+            }}
+          >
+            <input
+              type="text"
+              style={inputStyle}
+              value={userText}
+              onChange={(e) => setUserText(e.target.value)}
+              placeholder="What do you want to know human...."
+            />
+          </div>
+          <div>
+            <Button
+              fullWidth
+              variant="contained"
+              onClick={handleUserText}
+              sx={genButtonSx}
+              disabled={isLoading}
+            >
+              {!isLoading && "Ask"}
+              {isLoading && (
+                <CircularProgress sx={{ color: "rgba(255,255,255,0.7)" }} />
+              )}
+            </Button>
+          </div>
+        </div>
       </form>
-      <div className="absolute top-3 right-3">
-        <a target="_blank" rel="noopener noreferrer">
-          <img alt="yt" height={50} width={50} />
-        </a>
-        <div className="absolute top-0 bg-black/60" />
-      </div>
     </div>
   );
 };
 
 export default TextToSpeech;
+
+const inputStyle: React.CSSProperties = {
+  flexGrow: 1,
+  borderRadius: "0.8em",
+  color: "white",
+  paddingLeft: "1.2em",
+  backgroundColor: "#1B1A29",
+  width: "100%",
+  height: "60px",
+  fontSize: "1.3rem",
+};
+
+const genButtonSx: SxProps<Theme> = {
+  fontSize: "1.3rem",
+  paddingY: "0.7rem",
+  borderRadius: "1em",
+  boxShadow: 0,
+  textTransform: "none",
+  backgroundImage: "linear-gradient(to right,#280594 0%,#9306AB 100%)",
+  fontStyle: "italic",
+  fontWeight: "bold",
+  color: "white",
+};
+
+const fakeData: Message[] = [
+  { sender: "user", text: "Hi there!" },
+  { sender: "bot", text: "Hello! How can I assist you today?" },
+  { sender: "user", text: "Can you tell me a joke?" },
+  {
+    sender: "bot",
+    text: "Why did the scarecrow win an award? Because he was outstanding in his field!",
+  },
+];
--- src/pages/ChatBot/ChatBot.tsx
+++ src/pages/ChatBot/ChatBot.tsx
@@ -3,12 +3,12 @@ import { IsPlayingProvider } from "../../context/IsPlayingContext";
 import { ChatBotCanvas } from "../../components/ChatBotCanvas/ChatBotCanvas";
 
 export default function Home() {
-    return (
-        <main className="h-screen">
-            <IsPlayingProvider>
-                <TextToSpeech />
-                <ChatBotCanvas />
-            </IsPlayingProvider>
-        </main>
-    );
+  return (
+    <main className="h-full">
+      <IsPlayingProvider>
+        <TextToSpeech />
+        <ChatBotCanvas />
+      </IsPlayingProvider>
+    </main>
+  );
 }
